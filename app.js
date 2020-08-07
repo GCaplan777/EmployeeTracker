9dc178a96ab3506c18c36466d2819e7dea41c0a8
GCaplan <gabriel.caplan@gmail.com>
2020-08-07 04:08:28 -0400

    viewAllEmployees working in WorkBench. Code not running in JS yet

--- app.js
+++ app.js
@@ -67,7 +67,9 @@ const start = async () => {
 };
 
 // viewAllEmployees
-const viewAllEmployees = () => {};
+const viewAllEmployees = () => {
+  // query for the catogory Employees
+};
 
 // viewAllEmployeesByDept
 const viewAllEmployeesByDept = () => {};
@@ -86,3 +88,14 @@ const updateEmployeeRole = () => {};
 
 // updateEmployeeManager
 const updateEmployeeManager = () => {};
+
+function viewAllEmployees() {
+  connection.query(
+    "SELECT  employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department, CONCAT(manager.first_name,' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department on role.department_id = department.id  LEFT JOIN employee manager ON employee.manager_id = manager.id",
+    function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      start();
+    }
+  );
+}
--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -1,9 +1,10 @@
 INSERT INTO department (name)
-VALUES ('Production'), ('Finance'), ('Human Resource');
+VALUES ('Production'), ('Creative_Services'), ('Marketing');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('Manager_Production', 75000.50, 1), ('Analyst_Production', 70000, 1), ('SalesRep_Production', 65000, 1), ('Manager_Finance', 75000.50, 2), ('Analyst_Finance', 70000, 2), ('SalesRep_Finance', 60000, 2), ('Manager_HR', 75000.50, 3),
-('Analyst_HR', 70000, 3), ('SalesRep_HR', 65000, 3);
+VALUES ('Manager', 100000.50, 1), ('Engineer', 90000, 1), ('Developer', 85000, 1), ('Manager', 100000.50, 2), ('Analyst', 90000, 2), ('Graphic_Arts', 86000, 2), ('Manager', 100000.50, 3),
+('Advertising', 75000, 3), ('SalesSupervisor', 65000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Julius', 'Irving', 1, null), ('Moses', 'Malone', 2, 1), ('Maurice', 'Cheeks', 3, 1), ('Bobby', 'Jones', 4, null), ('Clemon', 'Johnson', 5, 4), ('George', 'Johnson', 6, 4), ('Clint', 'Richardson', 7, null), ('Sedale', 'Threatt', 8, 7), ('Andrew', 'Toney', 9, 7); 
+VALUES ('Julius', 'Irving', 1, null), ('Moses', 'Malone', 2, 1), ('Maurice', 'Cheeks', 3, 1), ('Bobby', 'Jones', 4, null), ('Clemon', 'Johnson', 5, 4), ('George', 'Johnson', 6, 4), ('Clint', 'Richardson', 7, null), ('Sedale', 'Threatt', 8, 7), 
+('Andrew', 'Toney', 9, 7), ('Sam', 'Williams', 5, 4), ('Leon', 'Wood', 2, 1);
